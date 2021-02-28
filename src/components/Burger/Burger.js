190efedf6194b7f4ce946efbc96f2f55bb05185f
Audrey Fleming <audreyfleming@gmail.com>
2021-02-28 12:39:48 -0500

    basic burger contructor

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+
+const burger = (props) => {
+	let newIngredients = Object.keys(props.ingredients).map( igKey => {
+			return [...Array(props.ingredients[igKey] )].map((_, i) => {
+				return <BurgerIngredient key={igKey + i} type={igKey} />
+			});
+		})
+		.flat();
+	if (newIngredients.length === 0) {
+		newIngredients = <p>Please start adding ingredients!</p>
+	}
+
+	return (
+		<div className={classes.Burger}>
+			<BurgerIngredient type="top-bread" />
+			{newIngredients}
+			<BurgerIngredient type="bottom-bread" />
+		</div>
+	);
+};
+
+export default burger;
